bed825838fa79132c498cf002e9ed4834a996249
qatar05896-maker <qatar05896@gmail.com>
2026-04-27 06:14:56 +0300

    Update autoend.py

--- AnnieXMedia/plugins/sudo/autoend.py
+++ AnnieXMedia/plugins/sudo/autoend.py
@@ -1,4 +1,5 @@
-﻿# Authored By Certified Coders © 2025
+# Authored By Certified Coders © 2026
+import re
 from pyrogram import filters
 from pyrogram.types import Message
 
@@ -6,20 +7,21 @@ from AnnieXMedia import app
 from AnnieXMedia.misc import SUDOERS
 from AnnieXMedia.utils.database import autoend_off, autoend_on
 
-
-@app.on_message(filters.command("autoend") & SUDOERS)
-async def auto_end_stream(_, message: Message):
-    usage = "<b>ᴇxᴀᴍᴘʟᴇ :</b>\n\n/autoend [ᴇɴᴀʙʟᴇ | ᴅɪsᴀʙʟᴇ]"
-    if len(message.command) != 2:
-        return await message.reply_text(usage)
-    state = message.text.split(None, 1)[1].strip().lower()
-    if state == "enable":
+@app.on_message(
+    filters.regex(r"^(تفعيل|إفعيل|ايقاف|إيقاف|تعطيل) (الانهاء التلقائي|الإنهاء التلقائي)$", flags=re.IGNORECASE) 
+    & filters.user(SUDOERS)
+)
+async def auto_end_stream_v2(_, message: Message):
+    input_text = message.text.lower()
+    
+    if "تفعيل" in input_text:
         await autoend_on()
         await message.reply_text(
-            "» ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴇɴᴀʙʟᴇᴅ.\n\nᴀssɪsᴛᴀɴᴛ ᴡɪʟʟ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇᴀᴠᴇ ᴛʜᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀғᴛᴇʀ ғᴇᴡ ᴍɪɴs ᴡʜᴇɴ ɴᴏ ᴏɴᴇ ɪs ʟɪsᴛᴇɴɪɴɢ."
+            "**تم تفعيل الإنهاء التلقائي.**\n\n**المساعد هيخرج من الكول لوحده لو مفيش حد بيسمع.**"
         )
-    elif state == "disable":
+        
+    elif "ايقاف" in input_text or "إيقاف" in input_text or "تعطيل" in input_text:
         await autoend_off()
-        await message.reply_text("» ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴅɪsᴀʙʟᴇᴅ.")
-    else:
-        await message.reply_text(usage)
+        await message.reply_text(
+            "**تم إيقاف الإنهاء التلقائي.**\n\n**المساعد هيفضل موجود في الكول حتى لو مفيش حد بيسمع.**"
+        )
