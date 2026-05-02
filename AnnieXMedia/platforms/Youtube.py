6653e3f085aa8de2c61694fddd1c3893f8c9d82a
qatar05896-maker <qatar05896@gmail.com>
2026-05-02 23:50:19 +0300

    Update Youtube.py

--- AnnieXMedia/platforms/Youtube.py
+++ AnnieXMedia/platforms/Youtube.py
@@ -31,8 +31,8 @@ class YouTubeAPI:
             "js_runtimes": {"node": {}},
             "remote_components": ["ejs:github"],
             "extractor_args": {
-            "youtube": {
-                    "client": ["android_vr"] 
+                "youtube": {
+                    "player_client": ["android_vr", "android"]
                 }
             }
         }
